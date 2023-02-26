c7b796c63278b6ec9654963d8a0f6327ea96c9f8
tememumtaza <reizants17@gmail.com>
2023-02-26 20:38:07 +0700

    Update FAHP.py

--- FAHP.py
+++ FAHP.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np 
+import openpyxl
 
 #Fungsi untuk membaca file dan menyimpan dalam bentuk array / tuple 
 def read_excel_file(df, n):
